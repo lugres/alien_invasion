c156776a06fb616ec358c391c097e12128fc5f73
Lugres <lugres@gmail.com>
2025-01-11 12:32:14 -0500

    Initial commit - basic game structure

---
+++ alien_invasion.py
@@ -0,0 +1,40 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+
+class AlienInvasion:
+    """A general class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initilize the game and create game resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Starting the main loop for the game."""
+        while True:
+            # Watching for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+
+            # Make the most recently drawn screen visisble.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
