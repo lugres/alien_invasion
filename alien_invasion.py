114e1850213967e4b95a691189c5a07e782a6741
Lugres <lugres@gmail.com>
2025-01-14 18:23:41 -0500

    Ensure bullets are hitting the aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,6 +132,14 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check for any bullets that have hit aliens
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update alien's positions."""
         self._check_fleet_edges()
--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 3.0
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
