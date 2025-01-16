6829b91e8131eb295ec1115fccc18e35cb255608
Lugres <lugres@gmail.com>
2025-01-16 11:42:06 -0500

    Speed up the game once the alien fleet is destroyed

--- settings.py
+++ settings.py
@@ -2,25 +2,40 @@ class Settings:
     """A class to store all settings for Alien Invasion game."""
 
     def __init__(self):
-        """Initilize the game's settings."""
+        """Initilize the game's static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 2.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 3.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
 
         # Alien settings
+        self.fleet_drop_speed = 10
+
+        # How quickly a game speeds up
+        self.speed_up_scale = 1.15
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 2.5
+        self.bullet_speed = 3.0
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 100
+
         # fleet_direction of 1 means right; -1 means left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speed_up_scale
+        self.bullet_speed *= self.speed_up_scale
+        self.alien_speed *= self.speed_up_scale
